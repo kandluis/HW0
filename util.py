cc69e1c9510b8e0f6e3949cbf680193515d3509e
Luis Perez <luisperez@college.harvard.edu>
2015-09-10 01:49:32 -0400

    Finished assignment and added new .gitignore file

--- util.py
+++ util.py
@@ -1,64 +1,102 @@
 ##### Filename: util.py
-##### Author: {your name}
-##### Date: {current date}
-##### Email: {your email}
+##### Author: Luis Perez
+##### Date: September 10, 2015
+##### Email: luisperez@college.harvar.edu
 
 import copy
 from collections import deque
 
 ## Problem 1
 
+def dot(x, y, i, j):
+    '''
+    Calculates the dot product of the i-th row and the j-th column of
+    x and y, respectively. Assumes matrices.
+    '''
+    return sum([x[i][k] * y[k][j] for k in xrange(len(x[0]))])
+
 def matrix_multiply(x, y):
-    pass
+    '''
+    Assumes X,Y are matrices represented as a list of rows,
+    where each row is a list of elements. Asserts inputs are
+    properly formatted matrices. An empty matrix is not acceptable input.
+    Returns the matrix X x Y.
+    Asserts error if dimensions do not match.
+    '''
+    assert len(x) > 0 and len(y > 0)
+    xrow, xcol = len(x), len(x[0])
+    yrow, ycol = len(y), len(y[0])
+    assert xcol == yrow
+    return [[dot(x, y, i, j) for j in xrange(ycol)] for i in xrange(xrow)]
 
 ## Problem 2, 3
 
 class MyQueue:
     def __init__(self):
-        pass
+        self.q = deque()
     def push(self, val):
-        pass
+        self.q.append(val)
     def pop(self):
-        pass
+        try:
+            return self.q.popleft()
+        except IndexError:
+            return None
     def __eq__(self, other):
-        pass
+        return isinstance(other, MyQueue) and self.q == other.q
     def __ne__(self, other):
-        pass
+        return not isinstance(other, MyQueue) or self.q != other.q
     def __str__(self):
-        pass
+        return "MyQueue:\n %s" % str(self.q)
 
 class MyStack:
     def __init__(self):
-        pass
+        self.s = deque()
     def push(self, val):
-        pass
+        self.s.append()
     def pop(self):
-        pass
+        try:
+            return self.s.pop()
+        except IndexError:
+            return None
     def __eq__(self, other):
-        pass
+        return isinstance(other, MyStack) and self.s == other.s
     def __ne__(self, other):
-        pass
+        return not isinstance(other, MyStack) or self.s != other.s
     def __str__(self):
-        pass
+        return "MyStack:\n %s" % str(self.s)
 
 ## Problem 4
 
 def add_position_iter(lst, number_from=0):
-    pass
+    '''
+    Essentially iteration, but much leaner than explicit for loops
+    '''
+    return [val + i + number_from for i,val enumerate(lst)]
 
 def add_position_recur(lst, number_from=0):
-    pass
+    if (len(lst) == 0):
+        return lst
+    else:
+        return [lst[0] + number_from] + add_position_recur(lst[1:], number_from + 1)
 
 def add_position_map(lst, number_from=0):
-    pass
+    return map(lambda (i, val) : val + i + number_from, enumerate(lst))
 
 ## Problem 5
 
 def remove_course(roster, student, course):
-    pass
+    '''
+    Raises KeyError if student is not present in roster.
+    Only removes coures present in student schedule, otherwise does nothing.
+    Mutates roster.
+    '''
+    roster[student].discard(course)
+
 
 ## Problem 6
 
 def copy_remove_course(roster, student, course):
-    pass
+    new_roster = copy.deepcopy(roster)
+    remove_course(new_roster, student, course)
+    return new_roster
 
